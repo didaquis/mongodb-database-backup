c90e46f9a7c1f68e6c7c009f28f60b7b5a18af75
Dídac García <didaquis@gmail.com>
2021-12-16 21:32:03 +0100

    Improved logger

--- src/helpers/logger.js
+++ src/helpers/logger.js
@@ -17,31 +17,23 @@ import log4js from 'log4js';
  */
 log4js.configure({
 	appenders: {
-		out:{ type: 'stdout' },
-		trace: { type: 'file', filename: 'logs/application_trace.log', maxLogSize: 204800, backups: 3, keepFileExt: true },
-		debug: { type: 'file', filename: 'logs/application_debug.log', maxLogSize: 204800, backups: 3, keepFileExt: true },
-		info: { type: 'file', filename: 'logs/application.log', maxLogSize: 204800, backups: 3, keepFileExt: true },
-		error: { type: 'file', filename: 'logs/application_error.log', maxLogSize: 204800, backups: 3, keepFileExt: true },
-		trace_filter: { type: 'logLevelFilter', appender: 'trace', level: 'trace', maxLevel: 'trace' },
-		debug_filter: { type: 'logLevelFilter', appender: 'debug', level: 'debug', maxLevel: 'debug' },
-		info_filter: { type:'logLevelFilter', appender: 'info', level: 'info' },
-		error_filter: { type:'logLevelFilter', appender: 'error', level: 'error' }
+		out: { type: 'stdout' },
 	},
 	categories: {
-		default: { appenders: [ 'trace_filter', 'debug_filter', 'info_filter', 'error_filter', 'out'], level: 'all' },
+		default: { appenders: ['out'], level: 'all' },
 	}
 });
 
 /**
- * Logger object that provides the methods to logger data (all logs are printed in console and in logs file).
+ * Logger object that provides the methods to logger data (all logs are printed in console)
  * @async
  * @example <caption>Usage of logger:</caption>
- *          logger.trace('trace'); 	// Log file: application_trace.log
- *          logger.debug('debug'); 	// Log file: application_debug.log
- *          logger.info('info'); 	// Log file: application.log
- *          logger.warn('warn'); 	// Log file: application.log
- *          logger.error('error'); 	// Log file: application_error.log and application.log
- *          logger.fatal('fatal'); 	// Log file: application_error.log and application.log
+ *          logger.trace('trace');
+ *          logger.debug('debug');
+ *          logger.info('info');
+ *          logger.warn('warn');
+ *          logger.error('error');
+ *          logger.fatal('fatal');
  */
 export const logger = log4js.getLogger();
 
